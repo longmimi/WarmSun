5b5acaf86fad685f893307e4c59347731c7b5162
STPace <heliapan@outlook.com>
2017-10-08 17:24:12 +0800

    更新home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,21 +1,41 @@
 <template>
   <div id="home">
     <div class="content">
-      <el-row>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-row :gutter="16">
+        <el-col :xs="24" :sm="16" :md="8" :lg="8"><div class="grid-content bg-purple">
+          <div class="block">
+            <el-carousel height="150px">
+              <el-carousel-item v-for="item in 3" :key="item">
+                <h3>政策{{ item }}</h3>
+              </el-carousel-item>
+            </el-carousel>
+          </div>
+        </div></el-col>
+        <el-col :xs="12" :sm="8" :md="4" :lg="4"><div class="grid-content bg-purple-dark">
+          <h3>志愿服务简介</h3>
+        </div></el-col>
+        <el-col :xs="12" :sm="8" :md="4" :lg="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :xs="12" :sm="8" :md="4" :lg="4"><div class="grid-content bg-purple-dark"></div></el-col>
+        <el-col :xs="12" :sm="8" :md="4" :lg="4"><div class="grid-content bg-purple">
+          <h3>天气</h3>
+        </div></el-col>
+        <el-col :xs="12" :sm="8" :md="4" :lg="4"><div class="grid-content bg-purple-dark">
+          <h3>家政服务简介</h3>
+        </div></el-col>
+        <el-col :xs="12" :sm="8" :md="4" :lg="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :xs="24" :sm="16" :md="8" :lg="8"><div class="grid-content bg-purple-dark">
+          <div class="block">
+            <el-carousel height="150px">
+              <el-carousel-item v-for="item in 2" :key="item">
+                <h3>活动{{ item }}</h3>
+              </el-carousel-item>
+            </el-carousel>
+          </div>
+        </div></el-col>
+        <el-col :xs="12" :sm="8" :md="4" :lg="4"><div class="grid-content bg-purple">
+          <h3>关于暖夕阳</h3>
+        </div></el-col>
+        <el-col :xs="12" :sm="8" :md="4" :lg="4"><div class="grid-content bg-purple-dark"></div></el-col>
       </el-row>
     </div>
   </div>
@@ -44,30 +64,35 @@ export default {
     width: 80%;
     margin: 10px auto;
   }
-  .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
   .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
+    margin: 10px 0;
   }
   .bg-purple {
     background: #d3dce6;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
   }
   .bg-purple-light {
     background: #e5e9f2;
   }
   .grid-content {
-    border-radius: 4px;
-    min-height: 220px;
+    border-radius: 0px;
+    min-height: 150px;
+    text-align: center;
   }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
+  .grid-content h3{
+    line-height: 150px;
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
   }
 </style>
